44c7c19b146184461c31654220c57bb3b7abdac1
Chido Nguyen <ChidoNguyen@users.noreply.github.com>
2018-02-01 15:25:05 -0800

    first commit

---
+++ room.c
@@ -0,0 +1,7 @@
+/*Chido Nguyen 931506965
+*Program 2: Adventures
+*Rooms: This file will house hard coded room names. It will also handle various rooms related functions. Generation of random room names.
+*Links rooms appropriately, functions will be specified prior to function defintion.
+*/
+
+enum Room_Name { Weight, Court, Spa, Pool, Locker, Closet, Sauna, Food, Security}
