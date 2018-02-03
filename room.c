d124d06b5e4e76c86fb91dfc4fcca1bcf5c43cd9
Chido Nguyen <nguychid@os1.engr.oregonstate.edu>
2018-02-02 20:18:48 -0800

    hardcoded room names

--- room.c
+++ room.c
@@ -5,16 +5,27 @@
 */
 
 #include <stdio.h>
-
+#include <string.h>
 /*
  *Room structures to handle data related to our rooms
+ *rm_name has size 9 because MAX 8 characters + null term 
  *cnx_cnt = connection count needs to stay below 6
  */
 struct ROOMS{
-	int cnx_cnt=0;
-}
+	char* rm_name;
+	int cnx_cnt;
+};
 
 //Main Function to test various usage Remove or comment out later//
 int main(){
+	//Names of all room outputs possible; 10 total//
+	char NAMES[10][9]= { "WEIGHTS", "COURT", "SPA", "SAUNA", "LOCKERS", "POOL", "STORE", "CLOSET", "FOODS", "GAINZ"};
+	
+	struct ROOMS one;
+	one.rm_name = NAMES[4];
+
+	
+	printf("Test Room: NAMES 1 : %s \n", one.rm_name );
+	
 	return 0;
 }
