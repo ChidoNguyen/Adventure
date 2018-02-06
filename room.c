0f7fc5f21fbb9d8426aff577973b7428bcbb7894
Chido Nguyen <chidonguyen@hotmail.com>
2018-02-05 17:54:25 -0800

    100% functional
    prints to file and all

--- room.c
+++ room.c
@@ -9,6 +9,7 @@
 #include <sys/stat.h> 	//required for mkdir
 #include <stdlib.h>		//this + time.h used for srand(time(null))
 #include <time.h>
+#include <fcntl.h>
 
 //GLOBAL VARIABLES//
 //Names of all room outputs possible; 10 total//
@@ -28,12 +29,15 @@ typedef enum {false, true } bool;
  *Room structures to handle data related to our rooms
  *rm_name has size 9 because MAX 8 characters + null term 
  *cnx_cnt = connection count needs to stay below 6
+ *roomLinks documents which room IDs are connected to it 
+ room_type; 0 start , 1 mid , 2 end
  */
 struct ROOMS{
 	char* rm_name;
 	int rm_ID;
 	int cnx_cnt;
 	int roomLinks[7];
+	int room_type;
 };
 
 
@@ -112,8 +116,8 @@ void ConnectRM(struct ROOMS* arr, int a, int b){
 	int a_idx, b_idx; // indexes for roomLinks array in struct
 	a_idx = arr[a].cnx_cnt;
 	b_idx = arr[b].cnx_cnt;
-	arr[a].roomLinks[a_idx] = b;
-	arr[b].roomLinks[b_idx] = a;
+	arr[a].roomLinks[a_idx] = arr[b].rm_ID;
+	arr[b].roomLinks[b_idx] = arr[a].rm_ID;
 	arr[a].cnx_cnt++;
 	arr[b].cnx_cnt++;
 }
@@ -125,7 +129,7 @@ Output:			True if room 1 is connected to room 2 already otherwise false
 bool ConnectionExist(struct ROOMS* arr, int a, int b){
 	int x;
 	for(x = 0; x < arr[a].cnx_cnt ; x++){
-		if( arr[a].roomLinks[x] == b )
+		if( arr[a].roomLinks[x] == arr[b].rm_ID )
 			return true;
 	}
 	return false;
@@ -141,16 +145,19 @@ void AddRandCon(struct ROOMS* arr){
 	int roomA;
 	int roomB;
 	
-	roomA = gamble(MAX_RM); // grab random room index //
-	while(CanConnect(arr, roomA) == false){
-		roomA= gamble(MAX_RM);
+	while(1){
+		roomA = gamble(MAX_RM); // grab random room index //
+		if(CanConnect(arr,roomA))
+			break;
+
 	}
-	
-	roomB= gamble(MAX_RM);
-	while(roomB == roomA || CanConnect(arr, roomB) ==  false || ConnectionExist(arr,roomA,roomB) == true){
-		roomB = gamble(MAX_RM);
+	do{
+		roomB= gamble(MAX_RM);
 	}
+	while(CanConnect(arr, roomB) ==  false || roomB == roomA ||  ConnectionExist(arr,roomA,roomB) == true);
 	ConnectRM(arr,roomA,roomB);
+
+
 	
 };
 
@@ -160,11 +167,11 @@ int main(){
 	srand( time( NULL)); // generates seed
 	//making our directory nguychid.rooms.PID//
 	//https://stackoverflow.com/questions/33332533/create-directory-and-store-file-c-programming//
-	/*char* src = "nguychid.rooms.";
-	char dirNameCombo[20];						// stores our DIRECTORY NAME!!!
+	char* src = "nguychid.rooms.";
+	char dirNameCombo[40];						// stores our DIRECTORY NAME!!!
 	sprintf(dirNameCombo,"%s%i", src, PID);		//combine username.rooms with PID
 	mkdir(dirNameCombo, 0755);					//create the dir
-	*/
+	
 	
 	
 	//Required to pick 7 of 10 rooms, use these numbers to represent our NAMES
@@ -193,22 +200,42 @@ int main(){
 	// loop through our array and set our room names 
 	struct ROOMS* layout;
 	layout = malloc(sizeof(struct ROOMS) * 7);
-	int x;
+	int x,j;
 	for (x = 0; x < 7 ; x++){
 		layout[x].rm_name = NAMES[rnd_name_idx[x]];
 		layout[x].rm_ID = rnd_name_idx[x];
 		layout[x].cnx_cnt = 0; // initialize connection counts
-		printf("%s\n", layout[x].rm_name); //printed out name as tracer check
+		layout[x].room_type = 1;
+		//printf("%s\n", layout[x].rm_name); //printed out name as tracer check
+	}
+	layout[ST_RM].room_type = 0;
+	layout[E_RM].room_type = 2;
+	//load up the rooms//
+	while(!NoVacancy(layout)){
+		AddRandCon(layout);
+	}
+	// WRITE TO FILES //
+	FILE* file;
+	char dirName_File[40];
+	//Loop all rooms//
+	for(x=0; x < MAX_RM ; x++){
+		//combine director + file name //
+		sprintf(dirName_File, "./%s/%s", dirNameCombo, layout[x].rm_name);
+		printf("%s\n", dirName_File);
+		file = fopen(dirName_File, "w" );
+		fprintf(file,"ROOM NAME: %s\n", layout[x].rm_name);
+		for(j = 0; j < layout[x].cnx_cnt; j++){
+			fprintf( file, "CONNECTION %i: %s\n", (j+1), NAMES[layout[x].roomLinks[j]]);
+		}
+		if(layout[x].room_type == 0)
+			fprintf(file, "ROOM TYPE: %s\n", "START_ROOM");
+		else if( layout[x].room_type == 1)
+			fprintf(file, "ROOM TYPE: %s\n", "MID_ROOM");
+		else
+			fprintf(file, "ROOM TYPE: %s\n", "END_ROOM");
+		fclose(file);
 	}
 	
-	printf("%i\n", NoVacancy(layout)); // function checked and works as intended
-	printf("Can connect check preset to 6: %i\n", CanConnect(layout, 3));
-	ConnectRM(layout,0,1);
-	for (x = 0; x < 2 ; x++){
-		printf("Room %i , connection count %i\n", layout[x].rm_ID, layout[x].cnx_cnt);
-		printf("%i\n",layout[x].roomLinks[0]);
-	}
-	printf("CONNECTION EXISTS %i\n", ConnectionExist(layout,0,1));
 	
 	
 	free(layout);
